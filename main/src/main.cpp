cdf5b5676b5cdc6d3186346c0fa39acdb22e2660
MAMMARU-class <144889491+MAMMARU-class@users.noreply.github.com>
2026-02-04 15:07:48 +0900

    there's some mistakes in theta calculation of IK

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -52,66 +52,18 @@ void setup(){
     neopixelWrite(RGB_BUILTIN, 0, 0, 255);
 }
 
-array<float, 3> foot2com_right = {0.0, 0.03, 0.158};
-array<float, 3> foot2com_left = {0.0, -0.03, 0.158};
+array<float, 3> foot2com_right = {0.05, 0.04, 0.138};
+array<float, 3> foot2com_left = {0.0, -0.03, 0.138};
 float theta = 0.0;
-
-float x_max = 0.03;
-float x_min = -0.03;
-
-float y_max = 0.02;
-float y_min = 0.0;
-
-float z_max = 0.158;
-float z_min = 0.138;
-
-float theta_max = 1.4;
-float theta_min = -0.2;
-
-array<float, 6> leg_motion;
-int step = 100;
+float a = 0.001;
 
 void loop(){
-    for(int i = 0; i<step; i++){
-        foot2com_right[0] = x_min + ( (x_max - x_min) * (float)(i) / (float)(step) );
-        foot2com_left[0]  = x_max - ( (x_max - x_min) * (float)(i) / (float)(step) );
+    // move legs with IK
+    Eglantyne.move_leg_ik(foot2com_right, theta, 0.0, true);
 
-        foot2com_right[1] = 0.03 + y_min + ( (y_max - y_min) * (float)(i) / (float)(step) );
-        foot2com_left[1] = -0.03 - (y_min + ( (y_max - y_min) * (float)(i) / (float)(step) ));
+    theta += a;
+    if(theta > 0.2){a = -0.001;}
+    else if (theta < -0.2){a = 0.001;}
 
-        // foot2com_right[2] = z_max - ( (z_max - z_min) * (float)(i) / (float)(step) );
-        // foot2com_left[2] = z_max - ( (z_max - z_min) * (float)(i) / (float)(step) );
-
-        // theta = theta_min + ( (theta_max - theta_min) * (float)(i) / (float)(step) );
-
-        Eglantyne.move_leg_ik(foot2com_right, theta, 0.0, true);
-        Eglantyne.move_leg_ik(foot2com_left, -theta, 0.0, false);
-        delay(10);
-    }
-    for(int i = 0; i<step; i++){
-        foot2com_right[0] = x_max - ( (x_max - x_min) * (float)(i) / (float)(step) );
-        foot2com_left[0]  = x_min + ( (x_max - x_min) * (float)(i) / (float)(step) );
-
-        foot2com_right[1] = 0.03 + y_max - ( (y_max - y_min) * (float)(i) / (float)(step) );
-        foot2com_left[1] = -0.03 - (y_max - ( (y_max - y_min) * (float)(i) / (float)(step) ));
-
-        // foot2com_right[2] = z_min + ( (z_max - z_min) * (float)(i) / (float)(step) );
-        // foot2com_left[2] = z_min + ( (z_max - z_min) * (float)(i) / (float)(step) );
-
-        // theta = theta_max - ( (theta_max - theta_min) * (float)(i) / (float)(step) );
-
-        Eglantyne.move_leg_ik(foot2com_right, theta, 0.0, true);
-        Eglantyne.move_leg_ik(foot2com_left, -theta, 0.0, false);
-        delay(10);
-    }
-
-    // foot2com_right[2] = 0.138;
-    // leg_motion = Eglantyne.leg_ik_solver_phi_zero(foot2com_right, 0.0, true);
-    // Serial.print("Leg right IK: ");
-    // for(int i=0; i<6; i++){
-    //     Serial.print(leg_motion[i]);
-    //     Serial.print(", ");
-    // }
-    // Serial.println("");
-    // delay(1000);
+    delay(10);
 }

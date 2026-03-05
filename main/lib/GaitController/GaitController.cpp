2b1038383003e3a6d73a5296bb655083ea115fa9
MAMMARU-class <144889491+MAMMARU-class@users.noreply.github.com>
2026-03-05 11:30:42 +0900

    fix START first foot pos

--- main/lib/GaitController/GaitController.cpp
+++ main/lib/GaitController/GaitController.cpp
@@ -96,7 +96,7 @@ void GaitController::init_state_variables(bool zero_start, bool zero_end)
 
     // last sprine at single -> double 
     if(zero_start){
-        cpn_d_0 = {-this->pn[0], -this->pn[1]};
+        cpn_d_0 = {-this->pn_p1[0], -this->pn_p1[1]};
         cvn_d_0 = {0.0f, 0.0f};
         can_d_0 = {0.0f, 0.0f};
         this->T_ds = this->T_ds/2;
